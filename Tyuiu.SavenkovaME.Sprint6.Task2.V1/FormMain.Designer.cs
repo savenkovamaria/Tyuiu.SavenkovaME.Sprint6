5645bcbe29c50425fe757652a14101bb20077501
Tyuiu.SavenkovaME@outlook.com <savenkova_mars@mail.ru>
2023-12-03 10:37:32 +0500

    Task2

--- Tyuiu.SavenkovaME.Sprint6.Task2.V1/FormMain.Designer.cs
+++ Tyuiu.SavenkovaME.Sprint6.Task2.V1/FormMain.Designer.cs
@@ -30,9 +30,9 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task2.V1
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBoxCondition_SME = new System.Windows.Forms.GroupBox();
             this.pictureBoxTask = new System.Windows.Forms.PictureBox();
             this.textBoxTask_SME = new System.Windows.Forms.TextBox();
@@ -113,6 +113,7 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task2.V1
             this.ColumnF});
             this.dataGridViewResultFunc_SME.Location = new System.Drawing.Point(11, 45);
             this.dataGridViewResultFunc_SME.Name = "dataGridViewResultFunc_SME";
+            this.dataGridViewResultFunc_SME.ReadOnly = true;
             this.dataGridViewResultFunc_SME.RowHeadersVisible = false;
             this.dataGridViewResultFunc_SME.RowHeadersWidth = 51;
             this.dataGridViewResultFunc_SME.RowTemplate.Height = 24;
@@ -124,6 +125,7 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task2.V1
             this.ColumnX.HeaderText = "X";
             this.ColumnX.MinimumWidth = 6;
             this.ColumnX.Name = "ColumnX";
+            this.ColumnX.ReadOnly = true;
             this.ColumnX.Width = 90;
             // 
             // ColumnF
@@ -131,22 +133,23 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task2.V1
             this.ColumnF.HeaderText = "F(X)";
             this.ColumnF.MinimumWidth = 6;
             this.ColumnF.Name = "ColumnF";
+            this.ColumnF.ReadOnly = true;
             this.ColumnF.Width = 90;
             // 
             // chartfunc_SME
             // 
-            chartArea5.Name = "ChartArea1";
-            this.chartfunc_SME.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chartfunc_SME.Legends.Add(legend5);
+            chartArea1.Name = "ChartArea1";
+            this.chartfunc_SME.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartfunc_SME.Legends.Add(legend1);
             this.chartfunc_SME.Location = new System.Drawing.Point(255, 45);
             this.chartfunc_SME.Name = "chartfunc_SME";
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series5.IsVisibleInLegend = false;
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chartfunc_SME.Series.Add(series5);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartfunc_SME.Series.Add(series1);
             this.chartfunc_SME.Size = new System.Drawing.Size(489, 399);
             this.chartfunc_SME.TabIndex = 1;
             this.chartfunc_SME.Text = "chartfunc";
