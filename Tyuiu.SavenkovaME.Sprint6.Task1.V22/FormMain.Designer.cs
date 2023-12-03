2ed1cff08f28668fd27140788df6d7a1a22a6fb8
Tyuiu.SavenkovaME@outlook.com <savenkova_mars@mail.ru>
2023-12-03 10:27:18 +0500

    Task1

--- Tyuiu.SavenkovaME.Sprint6.Task1.V22/FormMain.Designer.cs
+++ Tyuiu.SavenkovaME.Sprint6.Task1.V22/FormMain.Designer.cs
@@ -31,22 +31,22 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.groupBoxCondition_SME = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxTask_SME = new System.Windows.Forms.TextBox();
             this.groupBoxResult_SME = new System.Windows.Forms.GroupBox();
+            this.labelResult_SME = new System.Windows.Forms.Label();
+            this.textBoxResult_SME = new System.Windows.Forms.TextBox();
             this.groupBoxData_SME = new System.Windows.Forms.GroupBox();
+            this.labelStop_SME = new System.Windows.Forms.Label();
+            this.labelStart_SME = new System.Windows.Forms.Label();
+            this.textBoxStopStep_SME = new System.Windows.Forms.TextBox();
+            this.textBoxStartStep_SME = new System.Windows.Forms.TextBox();
             this.buttonHelp_SME = new System.Windows.Forms.Button();
             this.buttonDone_SME = new System.Windows.Forms.Button();
-            this.textBoxTask_SME = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBoxStartStep_SME = new System.Windows.Forms.TextBox();
-            this.textBoxStopStep_SME = new System.Windows.Forms.TextBox();
-            this.labelStart_SME = new System.Windows.Forms.Label();
-            this.labelStop_SME = new System.Windows.Forms.Label();
-            this.textBoxResult_SME = new System.Windows.Forms.TextBox();
-            this.labelResult_SME = new System.Windows.Forms.Label();
             this.groupBoxCondition_SME.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBoxResult_SME.SuspendLayout();
             this.groupBoxData_SME.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxCondition_SME
@@ -61,6 +61,29 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
             this.groupBoxCondition_SME.TabStop = false;
             this.groupBoxCondition_SME.Text = "Условие";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxTask_SME
+            // 
+            this.textBoxTask_SME.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxTask_SME.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTask_SME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTask_SME.Location = new System.Drawing.Point(6, 26);
+            this.textBoxTask_SME.Multiline = true;
+            this.textBoxTask_SME.Name = "textBoxTask_SME";
+            this.textBoxTask_SME.Size = new System.Drawing.Size(657, 273);
+            this.textBoxTask_SME.TabIndex = 0;
+            this.textBoxTask_SME.Text = "Протабулировать функцию на заданном диапазоне.\r\nРезультат вывести в виде таблицы." +
+    "";
+            // 
             // groupBoxResult_SME
             // 
             this.groupBoxResult_SME.Controls.Add(this.labelResult_SME);
@@ -72,6 +95,27 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
             this.groupBoxResult_SME.TabIndex = 1;
             this.groupBoxResult_SME.TabStop = false;
             this.groupBoxResult_SME.Text = "Вывод данных";
+            // 
+            // labelResult_SME
+            // 
+            this.labelResult_SME.AutoSize = true;
+            this.labelResult_SME.Location = new System.Drawing.Point(7, 27);
+            this.labelResult_SME.Name = "labelResult_SME";
+            this.labelResult_SME.Size = new System.Drawing.Size(102, 20);
+            this.labelResult_SME.TabIndex = 1;
+            this.labelResult_SME.Text = "Результат:";
+            // 
+            // textBoxResult_SME
+            // 
+            this.textBoxResult_SME.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxResult_SME.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxResult_SME.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult_SME.Location = new System.Drawing.Point(7, 51);
+            this.textBoxResult_SME.Multiline = true;
+            this.textBoxResult_SME.Name = "textBoxResult_SME";
+            this.textBoxResult_SME.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult_SME.Size = new System.Drawing.Size(343, 369);
+            this.textBoxResult_SME.TabIndex = 0;
             // 
             // groupBoxData_SME
             // 
@@ -87,6 +131,38 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
             this.groupBoxData_SME.TabStop = false;
             this.groupBoxData_SME.Text = "Ввод данных";
             this.groupBoxData_SME.Enter += new System.EventHandler(this.groupBoxData_SME_Enter);
+            // 
+            // labelStop_SME
+            // 
+            this.labelStop_SME.AutoSize = true;
+            this.labelStop_SME.Location = new System.Drawing.Point(208, 33);
+            this.labelStop_SME.Name = "labelStop_SME";
+            this.labelStop_SME.Size = new System.Drawing.Size(112, 20);
+            this.labelStop_SME.TabIndex = 1;
+            this.labelStop_SME.Text = "Конец шага:";
+            // 
+            // labelStart_SME
+            // 
+            this.labelStart_SME.AutoSize = true;
+            this.labelStart_SME.Location = new System.Drawing.Point(6, 31);
+            this.labelStart_SME.Name = "labelStart_SME";
+            this.labelStart_SME.Size = new System.Drawing.Size(113, 20);
+            this.labelStart_SME.TabIndex = 1;
+            this.labelStart_SME.Text = "Старт шага:";
+            // 
+            // textBoxStopStep_SME
+            // 
+            this.textBoxStopStep_SME.Location = new System.Drawing.Point(208, 54);
+            this.textBoxStopStep_SME.Name = "textBoxStopStep_SME";
+            this.textBoxStopStep_SME.Size = new System.Drawing.Size(191, 27);
+            this.textBoxStopStep_SME.TabIndex = 0;
+            // 
+            // textBoxStartStep_SME
+            // 
+            this.textBoxStartStep_SME.Location = new System.Drawing.Point(6, 54);
+            this.textBoxStartStep_SME.Name = "textBoxStartStep_SME";
+            this.textBoxStartStep_SME.Size = new System.Drawing.Size(196, 27);
+            this.textBoxStartStep_SME.TabIndex = 0;
             // 
             // buttonHelp_SME
             // 
@@ -112,82 +188,6 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
             this.buttonDone_SME.UseVisualStyleBackColor = false;
             this.buttonDone_SME.Click += new System.EventHandler(this.buttonDone_SME_Click);
             // 
-            // textBoxTask_SME
-            // 
-            this.textBoxTask_SME.BackColor = System.Drawing.SystemColors.Control;
-            this.textBoxTask_SME.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxTask_SME.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxTask_SME.Location = new System.Drawing.Point(6, 26);
-            this.textBoxTask_SME.Multiline = true;
-            this.textBoxTask_SME.Name = "textBoxTask_SME";
-            this.textBoxTask_SME.Size = new System.Drawing.Size(657, 273);
-            this.textBoxTask_SME.TabIndex = 0;
-            this.textBoxTask_SME.Text = "Протабулировать функцию на заданном диапазоне.\r\nРезультат вывести в виде таблицы." +
-    "";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(6, 87);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(320, 43);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // textBoxStartStep_SME
-            // 
-            this.textBoxStartStep_SME.Location = new System.Drawing.Point(6, 54);
-            this.textBoxStartStep_SME.Name = "textBoxStartStep_SME";
-            this.textBoxStartStep_SME.Size = new System.Drawing.Size(196, 27);
-            this.textBoxStartStep_SME.TabIndex = 0;
-            // 
-            // textBoxStopStep_SME
-            // 
-            this.textBoxStopStep_SME.Location = new System.Drawing.Point(208, 54);
-            this.textBoxStopStep_SME.Name = "textBoxStopStep_SME";
-            this.textBoxStopStep_SME.Size = new System.Drawing.Size(191, 27);
-            this.textBoxStopStep_SME.TabIndex = 0;
-            // 
-            // labelStart_SME
-            // 
-            this.labelStart_SME.AutoSize = true;
-            this.labelStart_SME.Location = new System.Drawing.Point(6, 31);
-            this.labelStart_SME.Name = "labelStart_SME";
-            this.labelStart_SME.Size = new System.Drawing.Size(113, 20);
-            this.labelStart_SME.TabIndex = 1;
-            this.labelStart_SME.Text = "Старт шага:";
-            // 
-            // labelStop_SME
-            // 
-            this.labelStop_SME.AutoSize = true;
-            this.labelStop_SME.Location = new System.Drawing.Point(208, 33);
-            this.labelStop_SME.Name = "labelStop_SME";
-            this.labelStop_SME.Size = new System.Drawing.Size(112, 20);
-            this.labelStop_SME.TabIndex = 1;
-            this.labelStop_SME.Text = "Конец шага:";
-            // 
-            // textBoxResult_SME
-            // 
-            this.textBoxResult_SME.BackColor = System.Drawing.SystemColors.Control;
-            this.textBoxResult_SME.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxResult_SME.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxResult_SME.Location = new System.Drawing.Point(7, 51);
-            this.textBoxResult_SME.Multiline = true;
-            this.textBoxResult_SME.Name = "textBoxResult_SME";
-            this.textBoxResult_SME.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxResult_SME.Size = new System.Drawing.Size(343, 369);
-            this.textBoxResult_SME.TabIndex = 0;
-            // 
-            // labelResult_SME
-            // 
-            this.labelResult_SME.AutoSize = true;
-            this.labelResult_SME.Location = new System.Drawing.Point(7, 27);
-            this.labelResult_SME.Name = "labelResult_SME";
-            this.labelResult_SME.Size = new System.Drawing.Size(102, 20);
-            this.labelResult_SME.TabIndex = 1;
-            this.labelResult_SME.Text = "Результат:";
-            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -198,17 +198,18 @@ namespace Tyuiu.SavenkovaME.Sprint6.Task1.V22
             this.Controls.Add(this.groupBoxData_SME);
             this.Controls.Add(this.groupBoxResult_SME);
             this.Controls.Add(this.groupBoxCondition_SME);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спринт 6 | Таск 1 | Вариант 22 | Савенкова М. Е.";
             this.groupBoxCondition_SME.ResumeLayout(false);
             this.groupBoxCondition_SME.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBoxResult_SME.ResumeLayout(false);
             this.groupBoxResult_SME.PerformLayout();
             this.groupBoxData_SME.ResumeLayout(false);
             this.groupBoxData_SME.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
